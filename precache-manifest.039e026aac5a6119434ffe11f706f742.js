70e09c54f403ae15a1d4125c63c1cee90c289144
Rishab Khanna <rishabkhanna27@gmail.com>
2020-09-11 14:36:26 +0530

    Updates

---
+++ precache-manifest.039e026aac5a6119434ffe11f706f742.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db6339d9854c8fa8332def742b38e62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b5d5d829991f752068b",
+    "url": "/static/css/main.f167c881.chunk.css"
+  },
+  {
+    "revision": "9aa827b828f6c2e56c85",
+    "url": "/static/js/2.d827aa3a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.d827aa3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b5d5d829991f752068b",
+    "url": "/static/js/main.c825440e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "2397232c4d8adafd60a339152861bff7",
+    "url": "/static/media/Capture.2397232c.PNG"
+  },
+  {
+    "revision": "efd4d3569d9cf7cc4be26f60482c46af",
+    "url": "/static/media/capture.efd4d356.svg"
+  },
+  {
+    "revision": "57c7368cad353a66c07eb0f42cf93249",
+    "url": "/static/media/cook.57c7368c.png"
+  },
+  {
+    "revision": "4101e2ff689d548155209f91428d2a6d",
+    "url": "/static/media/me.4101e2ff.jpg"
+  },
+  {
+    "revision": "e4a07a9276fa850b8f68a858a7860e50",
+    "url": "/static/media/music.e4a07a92.png"
+  },
+  {
+    "revision": "fb23340fe7591fa667f54f63c83e6239",
+    "url": "/static/media/starti.fb23340f.jpg"
+  },
+  {
+    "revision": "64c939de473cb62c7d741c6af18f6df8",
+    "url": "/static/media/wash.64c939de.png"
+  },
+  {
+    "revision": "8ffdd5cc736982ada9deecf1540a7eb4",
+    "url": "/static/media/wo.8ffdd5cc.png"
+  },
+  {
+    "revision": "44d145216f6595f979c37af1061d436d",
+    "url": "/static/media/wor.44d14521.png"
+  },
+  {
+    "revision": "9d204fb83662838ccc3ce91da5513617",
+    "url": "/static/media/work.9d204fb8.svg"
+  }
+]);
