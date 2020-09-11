fb7560a8561207a99586426eb29cff76536d06fc
Rishab Khanna <rishabkhanna27@gmail.com>
2020-09-11 14:23:42 +0530

    deploy

--- src/component/Work.js
+++ src/component/Work.js
@@ -1,5 +1,9 @@
 import React,{ Component } from 'react';
 import image from '../assets/work.svg';
+import image1 from '../assets/wash.png';
+import image2 from '../assets/cook.png';
+import image3 from '../assets/music.png';
+
 import { Button, Card } from 'react-bootstrap';
 import { Footer } from './Footer';
 
@@ -24,57 +28,17 @@ export class Work extends Component{
                 <div id="overview">
                     <div id="head1" className="complete1">
                         <div className="heading1">Complete Projects</div>
-                        <div class="complete2">
-                            <Card style={{ width: '20rem' }}>
-                              <Card.Img variant="top" src="holder.js/100px200" />
-                              <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                  Some quick example text to build on the card title and make up the bulk of
-                                  the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                              </Card.Body>
-                            </Card>
+                        <div class="complet">
+                            <div className="right-box1"><img className="pictu" src={image1} alt="imageing"/></div>
+                            <div className="left-box1">2</div>
                         </div>
-                        <div class="complete3">
-                        <Card style={{ width: '20rem' }}>
-                              <Card.Img variant="top" src="holder.js/100px200" />
-                              <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                  Some quick example text to build on the card title and make up the bulk of
-                                  the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                              </Card.Body>
-                            </Card>
+                        <div class="complet">
+                            <div className="right-box1">1</div>
+                            <div className="left-box1"><img className="pictu" src={image2} alt="imageing"/></div>
                         </div>
-                        <div class="complete4">
-                        <Card style={{ width: '20rem' }}>
-                              <Card.Img variant="top" src="holder.js/100px200" />
-                              <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                  Some quick example text to build on the card title and make up the bulk of
-                                  the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                              </Card.Body>
-                            </Card>
-                        </div>
-                        <div class="complete5">
-                        <Card style={{ width: '20rem' }}>
-                              <Card.Img variant="top" src="holder.js/100px200" />
-                              <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                  Some quick example text to build on the card title and make up the bulk of
-                                  the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                              </Card.Body>
-                            </Card>
+                        <div class="complet">
+                            <div className="right-box1"><img className="pictu" src={image3} alt="imageing"/></div>
+                            <div className="left-box1">2</div>
                         </div>
                     </div>
                 <div className="parallax3"></div>
@@ -178,6 +142,18 @@ export class Work extends Component{
                                 <Button variant="primary">Go somewhere</Button>
                               </Card.Body>
                             </Card></div>
+                        <div class="base7"><Card style={{ width: '19rem' }}>
+                              <Card.Img variant="top" src="holder.js/100px200" />
+                              <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                  Some quick example text to build on the card title and make up the bulk of
+                                  the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                              </Card.Body>
+                            </Card>
+                        </div>
                     </div>
                 <Footer/>
                 </div>
