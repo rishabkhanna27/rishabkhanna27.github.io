a4e489c9c69b08821f314e9b23f5f9fbbb169490
Rishab Khanna <44223499+rishabkhanna27@users.noreply.github.com>
2023-04-13 23:20:02 +0530

    Update NavBar.js
    hidden work page

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -13,7 +13,7 @@ export class NavBar extends Component{
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav>
             <NavLink className="navii let d-inline p-4 text-dark" to="/">About</NavLink>
-            <NavLink className="navii let d-inline p-4 text-dark" to="/Work">Work</NavLink>
+//             <NavLink className="navii let d-inline p-4 text-dark" to="/Work">Work</NavLink>
             </Nav>   
             </Navbar.Collapse>
             </Navbar>
